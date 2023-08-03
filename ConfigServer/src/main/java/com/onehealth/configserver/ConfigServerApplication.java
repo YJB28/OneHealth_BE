ae2732209025d5f56975ad40f6b271cbdbef04ae
YogeshBaiskar <yogeshjagdev@infobellit.com>
2023-08-03 18:55:18 +0530

    All added

---
+++ ConfigServer/src/main/java/com/onehealth/configserver/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.onehealth.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+	
+	@GetMapping
+	public String Welcome() {
+		
+		return "Welcome From OneHealth Team (OneHealth-ConfigServer)!!!";
+	}
+
+}
